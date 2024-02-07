a7ed4bc82d63bec955eacc9e13a6da3b85d7f45c
Anton Begehr <anton@begehr.me>
2024-02-07 15:49:49 +0100

    add json-apple dep and resolve deps

--- Package.swift
+++ Package.swift
@@ -9,12 +9,14 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.89.0"),
+        .package(url: "https://github.com/shareup/json-apple.git", from: "1.4.0"),
     ],
     targets: [
         .executableTarget(
             name: "App",
             dependencies: [
                 .product(name: "Vapor", package: "vapor"),
+                .product(name: "JSON", package: "json-apple"),
             ]
         ),
         .testTarget(name: "AppTests", dependencies: [
